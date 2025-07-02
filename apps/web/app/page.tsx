5f4667a662dae714d0a79ba24cc1e3f3b22f3f9f
Gabriel González <ggfresard@gmail.com>
2025-07-02 17:37:34 -0400

    aaa

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+import { GAME_CONSTANTS } from '@gard/shared'
+
 export default function Home() {
-    return <div>Hello World</div>
+    return <div>Hello World {GAME_CONSTANTS.CARDS_PER_PLAYER}</div>
 }
