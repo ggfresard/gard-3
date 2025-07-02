e9dad50f4964dba6a226891f5afa951c401cfa8c
Gabriel González <ggfresard@gmail.com>
2025-07-02 17:43:23 -0400

    sss

---
+++ packages/shared/src/constants.ts
@@ -0,0 +1,8 @@
+// Game constants
+export const GAME_CONSTANTS = {
+    MIN_PLAYERS: 2,
+    MAX_PLAYERS: 4,
+    CARDS_PER_PLAYER: 13,
+    TOTAL_CARDS: 52,
+    GAME_TIMEOUT: 30000, // 30 seconds
+} as const
